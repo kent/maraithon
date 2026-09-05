f082dd8233f3ba75c2d8cd6b8f8df0f714f23cf3
Kent <kent.fenwick@gmail.com>
2026-09-05 09:37:10 -0400

    style: self-host application fonts

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Inter var", "Inter", "ui-sans-serif", "system-ui", "sans-serif"],
+        sans: ["Inter", "ui-sans-serif", "system-ui", "sans-serif"],
         display: ['"Instrument Serif"', "ui-serif", "Georgia", "serif"]
       },
       colors: {
